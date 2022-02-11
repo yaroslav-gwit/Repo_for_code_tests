b8867fd557f8035cb90b5fe1a896ef4ce44e3dff
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-02-11 17:35:44 +0000

    Test

--- hr_prot/main.go
+++ hr_prot/main.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+
+	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 //VM status icons
@@ -24,6 +26,19 @@ func main() {
 			fmt.Println(vm_name)
 		}
 	}
+
+	//TABLE
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"#", "First Name", "Last Name", "Salary"})
+	t.AppendRows([]table.Row{
+		{1, "Arya", "Stark", 3000},
+		{20, "Jon", "Snow", 2000, "You know nothing, Jon Snow!"},
+	})
+	t.AppendSeparator()
+	t.AppendRow([]interface{}{300, "Tyrion", "Lannister", 5000})
+	t.AppendFooter(table.Row{"", "", "Total", 10000})
+	t.Render()
 }
 
 func vmList(plain ...bool) []string {
