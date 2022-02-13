7b7d1f5580bbd08e1bb29f91507a86223377057f
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-02-13 15:35:53 +0000

    Test

--- hr_prot/main.go
+++ hr_prot/main.go
@@ -174,7 +174,7 @@ func datasetsViper() interface{} {
 		panic(fmt.Errorf("fatal error config file: %w", viper_err))
 	}
 
-	datasetsViper := viper.GetStringSlice("datasets")
+	datasetsViper := viper.Get("datasets")
 
 	fmt.Println(reflect.TypeOf(datasetsViper))
 	fmt.Println(datasetsViper)
