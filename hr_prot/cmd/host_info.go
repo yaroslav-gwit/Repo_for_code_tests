6bc243ad5d49791b6485322fb8a88781fe683a16
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-02-19 23:36:26 +0000

    Test

--- hr_prot/cmd/host_info.go
+++ hr_prot/cmd/host_info.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/mem"
 	"github.com/spf13/cobra"
 )
@@ -17,15 +18,22 @@ var hostinfoCmd = &cobra.Command{
 	Short: "Show the host information",
 	Long:  `Show the host information in the form of table or json output`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("This will be a host info!")
-		// Hostname
-		hostname, _ := os.Hostname()
-		fmt.Println("Hostname: " + hostname)
-		// RAM
-		vmStat, _ := mem.VirtualMemory()
-		freeRam := vmStat.Available / 1024 / 1024 / 1024
-		totalRam := vmStat.Total / 1024 / 1024 / 1024
-		// fmt.Println("RAM:", freeRam, "GB", totalRam, "GB")
-		fmt.Printf("RAM: %dGB/%dGB\n", freeRam, totalRam)
+		hostInfo()
 	},
+}
+
+func hostInfo() {
+	fmt.Println("This will be a host info!")
+	// Hostname
+	hostname, _ := os.Hostname()
+	fmt.Println("Hostname: " + hostname)
+	// RAM
+	vmStat, _ := mem.VirtualMemory()
+	freeRam := vmStat.Available / 1024 / 1024 / 1024
+	totalRam := vmStat.Total / 1024 / 1024 / 1024
+	fmt.Printf("RAM: %dGB/%dGB\n", freeRam, totalRam)
+	//Uptime
+	hostUptime_, _ := host.Info()
+	hostUptime := hostUptime_.Uptime / 60
+	fmt.Printf("Uptime, minutes: %d", hostUptime)
 }
